781c6deb4fab6e8e9b0ab6793d8cd47ea9e21e7f
Milan Chauhan <milanchauhan.development@gmail.com>
2023-08-03 23:09:33 +0530

    Add Trusted By Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Featured from '../../components/featured/Featured';
 import "./Home.scss";
+import TrustedBy from '../../components/trustedBy/TrustedBy';
 
 const Home = () => {
     return (
         <div className='home'>
             <Featured />
+            <TrustedBy />
         </div>
     )
 }
