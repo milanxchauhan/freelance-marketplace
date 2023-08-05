e79713a42d12a1ea4357e959280702ec4c807fc9
Milan Chauhan <milanchauhan.development@gmail.com>
2023-08-05 23:44:29 +0530

    Import Slider

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./Slide.scss"
+import Slider from 'infinite-react-carousel'
 
-const Slide = () => {
+const Slide = ({children,slidesToShow,arrowsScroll}) => {
     return (
         <div className='slide'>
-            <div className="container"></div>
+            <div className="container">
+                <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+                    {children}
+                </Slider>
+
+            </div>
         </div>
     )
 }
