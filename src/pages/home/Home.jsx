c20e337edf8f3d1e9927d5c3b36434274e0ed536
Milan Chauhan <milanchauhan.development@gmail.com>
2023-08-07 22:49:03 +0530

    Add 2nd Feature Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -54,6 +54,32 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+
+            <div className="features dark">
+                <div className="container">
+                    <div className="item">
+                        <h1>fiverr business</h1>
+                        <h1>A business solution designed for teams</h1>
+                        <p>Upgrade to a curated experience  packed with tools and benefits, dedicated to businesses.</p>
+                        <div className="title">
+                            <img src="./img/check.png" alt="" />
+                            Access top freelancers and professional business tools for any project
+                        </div>
+                        <div className="title">
+                            <img src="./img/check.png" alt="" />
+                            Build your own branded marketplace of certified experts
+                        </div>
+                        <div className="title">
+                            <img src="./img/check.png" alt="" />
+                            Manage your freelance workforce and onboard additional talent with an end-to-end SaaS solution
+                        </div>
+                        <button>Explore Fiverr Business</button>
+                    </div>
+                    <div className="item">
+                        <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/51c35c7cecf75e6a5a0110d27909a2f5-1690202609364/EN.png" alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
