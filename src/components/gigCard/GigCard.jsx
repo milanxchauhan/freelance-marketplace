723f296d0bad1eec9703d7a4fc9d8aebff2cfaac
Milan Chauhan <milanchauhan.development@gmail.com>
2023-08-10 23:21:22 +0530

    Create GigCard.jsx

---
+++ src/components/gigCard/GigCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./GigCard.scss"
+import { Link } from 'react-router-dom'
+
+const GigCard = ({item}) => {
+    return (
+        <Link to="/gig/123">
+            <div className='gigCard'>
+                <img src={item.img} alt="" />
+                <div className="info">
+                    <div className="user">
+                        <img src={item.pp} alt="" />
+                        <span>{item.username}</span>
+                    </div>
+                    <p>{item.desc}</p>
+                    <div className="star">
+                        <img src="./img/star.png" alt="" />
+                        <span>{item.star}</span>
+                    </div>
+                </div>
+                <div className="details"></div>
+            </div>
+        </Link>
+    )
+}
+
+export default GigCard
