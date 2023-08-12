b5d6fe795bb61f6af4fa9852ae2c7ce7873361cc
Milan Chauhan <milanchauhan.development@gmail.com>
2023-08-12 23:55:06 +0530

    Update Gigs.jsx

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 import "./Gigs.scss"
 
 const Gigs = () => {
+
+    const[open, setOpen] = useState(false)
+
     return (
-        <div className='gigs'>Gigs</div>
+        <div className='gigs'>
+            <div className="container">
+                <span className="breadcrumbs">FIVER &gt; GRAPHICS & DESIGN &gt;</span>
+                <h1>AI Artists</h1>
+                <p>
+                    Explore the boundaries of art and technology with Fiverr's AI artists
+                </p>
+                <div className="menu">
+                    <div className="left">
+                        <span>Budget</span>
+                        <input type="text" placeholder='min'/>
+                        <input type="text" placeholder='max'/>
+                        <button>Apply</button>
+                    </div>
+                    <div className="right">
+                        <span className="sortBy">SortBy</span>
+                        <span className="sortType">Best Selling</span>
+                        <img src="./img/down.png" alt="" onClick={()=>setOpen(!open)} />
+                        {open && (
+                            <div className="rightMenu">
+                                <span>Newest</span>
+                                <span>Best Selling</span>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
